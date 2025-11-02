9966a621c3af0b3870796ee1d2096ddad9a381a5
jed | Cursor.sh | claude-3-5-sonnet-20241022 <cursor@cursor.sh>
2025-11-01 23:08:21 -0400

    Set CXXFLAGS as environment variable for make
    Configure wasn't passing CXXFLAGS through to actual compilation.
    Setting CXXFLAGS as environment variable before make ensures it's used.
    
    Filename: docker-build-and-package.sh
    Fix: Export CXXFLAGS before running make

--- docker-build-and-package.sh
+++ docker-build-and-package.sh
@@ -28,6 +28,7 @@ ${SOURCE_DIR}/configure \
 
 # Build
 echo "Building GCC (this may take several hours)..."
+export CXXFLAGS="-std=c++14 -Wno-error=literal-suffix -Wno-error=cast-function-type -Wno-error=implicit-fallthrough"
 make -j$(nproc)
 
 # Install to staging directory
